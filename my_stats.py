3ee921b75472c995aed0dc670d1c4ed37c1db6bc
Patrick Kennedy <pmk2109@gmail.com>
2016-05-23 10:39:09 -0500

    up to speed

--- my_stats.py
+++ my_stats.py
@@ -1,6 +1,7 @@
 
-
-
+from random import random
+from collections import defaultdict
+from operator import itemgetter
 
 
 class PMF(object):
@@ -29,3 +30,57 @@ class PMF(object):
 
     def print_pmf(self):
         print self.d
+
+
+
+
+class RV(object):
+    def __init__(self, pmf_obj):
+        self.pmf_obj = pmf_obj
+
+
+    def sample(self):
+        proportions = defaultdict(float)
+        old_v = 0.
+        r = random()
+        keys = []
+        vals = []
+
+        for k, v in self.pmf_obj.d.iteritems():
+            old_v += v
+            vals.append(old_v)
+            keys.append(k)
+
+        big_list = [list(x) for x in zip(*sorted(zip(keys, vals), key=itemgetter(0)))]
+        new_keys = big_list[0]
+        new_vals = big_list[1]
+
+        for k, v in zip(new_keys, new_vals):
+            if r >= v:
+                pass
+            else:
+                return k
+
+
+    def all_outcomes(self):
+        return self.pmf_obj.d.keys()
+
+
+    def pmf(self):
+        return self.pmf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #
